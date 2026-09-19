69b342b319c503aee61d5746e42fc61cc536eccb
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-19 20:28:35 +0530

    Create A_23_Find the Running Median.java

---
+++ A_23_Find the Running Median.java
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'runningMedian' function below.
+ *
+ * The function is expected to return a DOUBLE_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+vector<double> runningMedian(vector<int> a) {
+    // Max-heap to store the smaller half of numbers
+    priority_queue<int> max_heap;
+    
+    // Min-heap to store the larger half of numbers
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+    
+    vector<double> medians;
+    medians.reserve(a.size());
+
+    for (int num : a) {
+        // Step 1: Add element to appropriate heap
+        if (max_heap.empty() || num <= max_heap.top()) {
+            max_heap.push(num);
+        } else {
+            min_heap.push(num);
+        }
+
+        // Step 2: Rebalance heaps so size difference is at most 1
+        if (max_heap.size() > min_heap.size() + 1) {
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        } else if (min_heap.size() > max_heap.size()) {
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+        }
+
+        // Step 3: Compute median
+        if (max_heap.size() > min_heap.size()) {
+            medians.push_back((double)max_heap.top());
+        } else {
+            medians.push_back((max_heap.top() + min_heap.top()) / 2.0);
+        }
+    }
+
+    return medians;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string a_count_temp;
+    getline(cin, a_count_temp);
+
+    int a_count = stoi(ltrim(rtrim(a_count_temp)));
+
+    vector<int> a(a_count);
+
+    for (int i = 0; i < a_count; i++) {
+        string a_item_temp;
+        getline(cin, a_item_temp);
+
+        int a_item = stoi(ltrim(rtrim(a_item_temp)));
+
+        a[i] = a_item;
+    }
+
+    vector<double> result = runningMedian(a);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
